5266caae7fffdcff64cfd88b49bda757ebd82e9d
Gokul NC <gokulnc@ymail.com>
2019-11-06 14:23:12 +0000

    Add a few more langs

---
+++ downloads/gujarati/downloader.sh
@@ -0,0 +1,29 @@
+echo Downloading files...
+
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2012.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2013_20.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2013_48.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_15.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_23.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_35.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_41.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_42.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_49.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2014_52.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_06.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_11.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_14.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_18.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_22.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_27.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_32.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_35.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_40.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2015_48.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2016_30.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2016_50.raw.xz
+axel http://web-language-models.s3-website-us-east-1.amazonaws.com/ngrams/gu/raw/gu.2017_17.raw.xz
+
+echo Extracting files...
+unxz -kv *.xz
+# rm *.xz
---
+++ logs/README.md
@@ -0,0 +1,17 @@
+## Collected Stats for Indian Languages
+
+## Collected Stats for Indian Languages
+
+
+| Language | Unique Tokens | Unique Sentences | Total Tokens |
+| --------:|:-------------:|:----------------:|:------------:|
+| Bengali  | 1.3 million   | 5.6 million      | 57.6 million |
+| Gujarati | 1.3 million | 3.4 million | 43 million |
+| Hindi |
+| Kannada | 2.8 million | 3.8 million | 42 million |
+| Malayalam | 7.8 million | 9.5 million | 96 million |
+| Marathi | 2.2 million | 5.5 million | 65.5 million |
+| Oriya | 72k | 44k | 0.43 million |
+| Punjabi | 261k | 783k | 9.3 million |
+| Tamil | 10.9 million | 29 million | 300 million |
+| Telugu |  3.4 million | 5.7 million | 60 million |
